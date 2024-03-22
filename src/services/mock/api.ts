a0c8eaa847d2e781100aacc7d31b6398e950f5c9
DreamyTZK <i@tzki.cn>
2024-03-22 17:46:40 +0800

    feat: 添加request

--- src/services/mock/api.ts
+++ src/services/mock/api.ts
@@ -1,5 +1,4 @@
-import type { GoodsDetail } from '@kkservice/types'
-import { request } from '@kkservice/fetch'
+import { request } from '@/lib/request'
 
 export function goodsDetailApi(commonId: string, goodsId?: string | null) {
   const urlencoded = new URLSearchParams()
@@ -7,7 +6,7 @@ export function goodsDetailApi(commonId: string, goodsId?: string | null) {
   if (goodsId)
     urlencoded.append('goodsId', goodsId)
 
-  return request<GoodsDetail>('https://mock.apifox.com/m1/4081920-0-default/goods', {
+  return request('https://mock.apifox.com/m1/4081920-0-default/goods', {
     method: 'post',
     body: urlencoded,
     cache: 'no-cache',
