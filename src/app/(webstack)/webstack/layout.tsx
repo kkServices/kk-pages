f28f7fa722aa6724c49e637c170e72575275e617
DreamyTZK <i@tzki.cn>
2024-03-28 16:50:43 +0800

    feat: 添加seo信息

--- src/app/(webstack)/webstack/layout.tsx
+++ src/app/(webstack)/webstack/layout.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import type { Metadata } from 'next'
 import { StyleProvider } from './Provider/Style'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | 小康的自留地',
+    default: '小康的网址收藏 | 小康的自留地', // a default is required when creating a template
+  },
+}
 
 interface LayoutProps {
   children: React.ReactNode
