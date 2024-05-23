8dcfaf536777f1b0fbb82f75869b22977db2a5dd
DreamyTZK <i@tzki.cn>
2024-05-23 13:55:40 +0800

    chore: eslint

--- src/app/(webstack)/webstack/provider/Style.tsx
+++ src/app/(webstack)/webstack/provider/Style.tsx
@@ -6,7 +6,8 @@ import { useServerInsertedHTML } from 'next/navigation'
 interface StyleProviderProps {
   children: React.ReactNode
 }
-export function StyleProvider(StyleProvider: StyleProviderProps) {
+export function StyleProvider(styleProviderProps: StyleProviderProps) {
+  const { children } = styleProviderProps
   const inserted = useRef(false)
   useServerInsertedHTML(() => {
     const styleText = `html,body{ height: 100%;}
@@ -25,6 +26,7 @@ export function StyleProvider(StyleProvider: StyleProviderProps) {
     inserted.current = true
 
     return (
+    // eslint-disable-next-line react-dom/no-dangerously-set-innerhtml
       <style
         id="webstack-global-style"
         data-rc-order="prepend"
@@ -34,10 +36,5 @@ export function StyleProvider(StyleProvider: StyleProviderProps) {
     )
   })
 
-  return (
-    <>
-      {/* eslint-disable-next-line react/prefer-destructuring-assignment */}
-      {StyleProvider.children}
-    </>
-  )
+  return children
 }
